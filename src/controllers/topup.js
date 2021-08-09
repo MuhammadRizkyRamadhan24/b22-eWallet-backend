fd4906f9e49583c66fe15b678fb0317a1033d3f8
MuhammadRizkyRamadhan24 <zidan.muh69@gmail.com>
2021-08-09 18:51:19 +0700

    update

--- src/controllers/topup.js
+++ src/controllers/topup.js
@@ -8,8 +8,12 @@ exports.createTopup = async (req, res) => {
   if (typeof req.body.deductedBalance === 'string') {
     req.body.deductedBalance = parseInt(req.body.deductedBalance)
   }
+  const data = {
+    ...req.body,
+    userId: id
+  }
   if (user) {
-    const trx = await TopupModel.create(req.body)
+    const trx = await TopupModel.create(data)
     const total = req.body.deductedBalance
     user.set('balance', user.balance + total)
     await user.save()
